31153d4b190d08fed4bf263eef4177bc793577b4
KhansaAhmed88013 <balqeesriaz880@gmail.com>
2026-06-27 19:13:34 +0500

    user updated

--- src/pages/AdminUsers.jsx
+++ src/pages/AdminUsers.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router-dom'
 
 const roles = [
   { value: 'MACHINE_OPERATOR', label: 'Machine Operator' },
-  { value: 'MAINTENANCE_ENGINEER', label: 'Maintenance Engineer' },
   { value: 'SYSTEM_ADMIN', label: 'System Admin' },
 ]
 
